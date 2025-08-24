2605ab30772eb7ac1f6f65daff729452831153fc
jakubbbdev <veritycsgo@gmail.com>
2025-08-24 16:07:43 +0200

    refactor(api, ui): remove unused API hooks and UserDropdown component; clean up `useSynchronizedAnimation` for better readability

--- panel/src/lib/hooks/animation/useSynchronizedAnimation.ts
+++ panel/src/lib/hooks/animation/useSynchronizedAnimation.ts
@@ -1,37 +1,35 @@
-//Thanks to: https://github.com/samselikoff/2022-02-24-use-synchronized-animation
-
-import { useLayoutEffect, useRef } from 'react';
+import {useLayoutEffect, useRef} from 'react';
 
 let stashedTime: CSSNumberish | null = null;
 
 export function useSynchronizedAnimation<T>(animationName: string) {
-  let ref = useRef<T | null>(null);
+    let ref = useRef<T | null>(null);
 
-  useLayoutEffect(() => {
-    let animations = document
-      .getAnimations()
-      //@ts-ignore
-      .filter((animation) => animation.animationName === animationName);
+    useLayoutEffect(() => {
+        let animations = document
+            .getAnimations()
+            //@ts-ignore
+            .filter((animation) => animation.animationName === animationName);
 
-    let myAnimation = animations.find(
-      //@ts-ignore
-      (animation) => animation.effect?.target === ref.current
-    );
+        let myAnimation = animations.find(
+            //@ts-ignore
+            (animation) => animation.effect?.target === ref.current
+        );
 
-    if (myAnimation === animations[0] && stashedTime) {
-      myAnimation.currentTime = stashedTime;
-    }
+        if (myAnimation === animations[0] && stashedTime) {
+            myAnimation.currentTime = stashedTime;
+        }
 
-    if (myAnimation && myAnimation !== animations[0]) {
-      myAnimation.currentTime = animations[0].currentTime;
-    }
+        if (myAnimation && myAnimation !== animations[0]) {
+            myAnimation.currentTime = animations[0].currentTime;
+        }
 
-    return () => {
-      if (myAnimation && myAnimation === animations[0]) {
-        stashedTime = myAnimation.currentTime;
-      }
-    };
-  }, [animationName]);
+        return () => {
+            if (myAnimation && myAnimation === animations[0]) {
+                stashedTime = myAnimation.currentTime;
+            }
+        };
+    }, [animationName]);
 
-  return ref;
+    return ref;
 }
